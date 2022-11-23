d5e4c0336c31fdf1696ef75f30f97d6b6c4e8c2d
blackVoid00 <nouhailanouss@gmail.com>
2022-11-23 09:00:33 +0100

    Adding charts

--- src/components/Home2.js
+++ src/components/Home2.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled from "styled-components"
 import mission from '../assets/mission.png'
+import {Bar} from 'react-chartjs-2';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const DivHome=styled.div`
     margin-top:150px;
@@ -8,11 +12,27 @@ const DivHome=styled.div`
    
 `
 const Home2 = () => {
+  const url="https://localhost:7111/api/Missions"
+  const [missions,setMissions]=useState([])
+  useEffect(()=>{
+    axios.get(url).then((response) =>{
+      setMissions(response.data)
+
+    })
+  })
     const identifant=localStorage.getItem('loginId');
+    const [missionStats,setMissionStats] =useState({
+       labels:missions.map((m)=>m.dateCreation),
+       datasets:[{
+        label:"Missions par Nature",
+        data:missions.map((m)=>m.dureeIntervention)
+       }]
+      })
   return (
     <DivHome>
         {/* <h1 style={{fontSize:"45px",color:"black",fontWeight:"bold"}}>Bienvenue</h1> */}
         <img style={{maxWidth:"100%"}} src={mission}></img>
+        <Bar data={missionStats}></Bar>
     </DivHome>
   )
 }
