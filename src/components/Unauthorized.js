226d9c1e2b0fda9a2413a99f4b03bf0ab5cdb909
blackVoid00 <nouhailanouss@gmail.com>
2023-01-13 17:22:22 +0100

    styling and adding role based routes to the unauthorized page

--- src/components/Unauthorized.js
+++ src/components/Unauthorized.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import { H1 } from './StyleMsC'
-
+import { Button, ButtonM, H1 } from './StyleMsC'
+import logo from '../assets/logo.png'
+import { useNavigate } from 'react-router-dom'
 const Unauthorized = () => {
+  const navigate=useNavigate()
+  const nom=localStorage.getItem('nom')
+  const prenom=localStorage.getItem('prenom')
+  const GoDash=()=>{
+    const role=localStorage.getItem('role')
+    if(role=="0"){
+      navigate('/listmsUser')
+    }
+    if(role=="1"){
+      navigate('/app')
+    }
+    if(role=="2"){
+      navigate('/app')
+    }
+  }
   return (
-    <div>
-      <H1>ERROR 404 YOU CANNOT ACCESS THIS LINK</H1>
+    <div style={{margin:"230px 430px 130px 430px"}}>
+      <h1 style={{marginLeft:"380px",fontSize:"80px",color:"black"}}> 404 </h1>
+      <br></br> <br></br>
+      <h2 style={{color:"black"}}>Vous n'êtes pas autorisé à accéder à cette ressource {nom} {prenom}</h2>
+      <br></br> <br></br>
+      <div style={{marginLeft:"260px"}}>
+
+      <Button onClick={GoDash}>Mon Dashboard</Button>
+      </div>
+   
+      <br></br> <br></br>
+     
     </div>
   )
 }
